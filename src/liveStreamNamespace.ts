f1efe69f252b0cb09e579a32d59d34ce79c95a69
michal-landair <michal.landair@gmail.com>
2025-02-04 17:15:33 +0200

    add socket for response

--- src/liveStreamNamespace.ts
+++ src/liveStreamNamespace.ts
@@ -18,10 +18,10 @@ export const setupDeviceStatusNamespace = (io: SocketIOServer) => {
       console.log(`Device ${deviceId} connected`);
       deviceStatusMap.set(deviceId, true); // עדכון הסטטוס במפה
       socketToDeviceMap.set(socket.id, deviceId); // שמירת מיפוי בין הסוקט ל-deviceId
-      await updateDeviceStatusInDB(deviceId, "Active"); // עדכון ב-DB
+      await updateDeviceStatusInDB(deviceId, "Active", "Clear"); // עדכון ב-DB עם סטטוס אזור "Clear"
     
       // שליחה ללקוח על עדכון הסטטוס
-      io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'Active' });
+      io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'Active', statusArea: 'Clear' });
     });
     
     socket.on('disconnect', async () => {
@@ -30,26 +30,39 @@ export const setupDeviceStatusNamespace = (io: SocketIOServer) => {
       if (deviceId) {
         deviceStatusMap.set(deviceId, false); // עדכון הסטטוס במפה
         socketToDeviceMap.delete(socket.id); // הסרת המיפוי
-        await updateDeviceStatusInDB(deviceId, "InActive"); // עדכון ב-DB
+        await updateDeviceStatusInDB(deviceId, "InActive", "InActive"); // עדכון ב-DB עם סטטוס אזור "InActive"
         console.log(`Device ${deviceId} set to InActive`);
     
         // שליחה ללקוח על עדכון הסטטוס
-        io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'InActive' });
+        io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'InActive', statusArea: 'InActive' });
       }
     });
     
+    // עדכון סטטוס אזור לפי תמונה
+    socket.on('statusAreaUpdate', async (deviceId: string, statusArea: string) => {
+      console.log(`Image received for device ${deviceId}, statusArea: ${statusArea}`);
+      
+      if (deviceStatusMap.get(deviceId)) { // אם המכשיר פעיל
+        await updateDeviceStatusInDB(deviceId, "Active", statusArea); // עדכון ב-DB עם סטטוס האזור החדש
+        io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'Active', statusArea });
+      } else { // אם המכשיר לא פעיל
+        await updateDeviceStatusInDB(deviceId, "InActive", "InActive"); // עדכון ב-DB עם סטטוס אזור לא פעיל
+        io.of('/deviceStatus').emit('statusUpdate', { _id: deviceId, status: 'InActive', statusArea: "InActive" });
+      }
+    });
   });
 };
 
-async function updateDeviceStatusInDB(deviceId: string, status: string): Promise<void> {
+async function updateDeviceStatusInDB(deviceId: string, status: string, statusArea: string): Promise<void> {
   const url = `https://api-service-hab9fmgne7dxa5ad.italynorth-01.azurewebsites.net/api/device/${deviceId}`;
   // const url = `http://localhost:8080/api/device/${deviceId}`;
 
   try {
     console.log(url);
     console.log(status);
-    const response = await axios.put(url, { status });
-    console.log(`Device ${deviceId} status updated to ${status}`);
+    console.log(statusArea);
+    const response = await axios.put(url, { status, statusArea });
+    console.log(`Device ${deviceId} status updated to ${status}, statusArea: ${statusArea}`);
     console.log(response.data);
   } catch (error: unknown) { // הגדרת טיפוס של 'unknown' כדי לבדוק אותו
     if (error instanceof Error) { // בדיקה אם error הוא אובייקט מסוג Error
@@ -59,4 +72,3 @@ async function updateDeviceStatusInDB(deviceId: string, status: string): Promise
     }
   }
 }
-
